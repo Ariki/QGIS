149306bfbee49e78278a86f20bcc9207d953c917
Ariki <ardjakov@ardageo.ru>
2015-02-07 13:48:51 +0300

    Fix PyQt4 configure-ng issues

--- cmake/FindPyQt.py
+++ cmake/FindPyQt.py
@@ -1,75 +1,64 @@
-# -*- coding: utf-8 -*-
-#
-#   Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
-#    All rights reserved.
-#
-#    Redistribution and use in source and binary forms, with or without
-#    modification, are permitted provided that the following conditions are met:
-#        * Redistributions of source code must retain the above copyright
-#        notice, this list of conditions and the following disclaimer.
-#        * Redistributions in binary form must reproduce the above copyright
-#        notice, this list of conditions and the following disclaimer in the
-#        documentation and/or other materials provided with the distribution.
-#        * Neither the name of the  Simon Edwards <simon@simonzone.com> nor the
-#        names of its contributors may be used to endorse or promote products
-#        derived from this software without specific prior written permission.
-#
-#    THIS SOFTWARE IS PROVIDED BY Simon Edwards <simon@simonzone.com> ''AS IS'' AND ANY
-#    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-#    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-#    DISCLAIMED. IN NO EVENT SHALL Simon Edwards <simon@simonzone.com> BE LIABLE FOR ANY
-#    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-#    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-#    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-#    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-#    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# FindPyQt.py
 # Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
+# Copyright (c) 2014, Raphael Kubo da Costa <rakuco@FreeBSD.org>
+# Copyright (c) 2014, Alexei Ardyakov <ardjakov@rambler.ru>
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-try:
-    import PyQt4.pyqtconfig
-    pyqtcfg = PyQt4.pyqtconfig.Configuration()
-    pyqt_sip_flags = pyqtcfg.pyqt_sip_flags
-    print("pyqt_version:%06.0x" % pyqtcfg.pyqt_version)
-    print("pyqt_version_num:%d" % pyqtcfg.pyqt_version)
-    print("pyqt_version_str:%s" % pyqtcfg.pyqt_version_str)
-except ImportError:
-    # PyQt built with configure-ng.py has no pyqtconfig module
-    from PyQt4.QtCore import PYQT_CONFIGURATION, PYQT_VERSION, PYQT_VERSION_STR
-    pyqtcfg = None
-    pyqt_sip_flags = PYQT_CONFIGURATION["sip_flags"]
-    print("pyqt_version:%06.0x" % PYQT_VERSION)
-    print("pyqt_version_num:%d" % PYQT_VERSION)
-    print("pyqt_version_str:%s" % PYQT_VERSION_STR)
+import PyQt4.QtCore
+import os
+import sys
 
-pyqt_version_tag = ""
-in_t = False
-for item in pyqt_sip_flags.split(' '):
-    if item=="-t":
-        in_t = True
-    elif in_t:
-        if item.startswith("Qt_4"):
-            pyqt_version_tag = item
+
+def get_default_sip_dir():
+    # This is based on QScintilla's configure.py, and only works
+    # for a few popular locations of PyQt SIP directory.
+    if sys.platform == 'win32':
+        import site
+        default_sip_dirs = [
+            os.path.join(p, 'PyQt4\\sip\\PyQt4')
+            for p in site.getsitepackages()
+            ]
     else:
-        in_t = False
-print("pyqt_version_tag:%s" % pyqt_version_tag)
-
-if pyqtcfg:
-    print("pyqt_mod_dir:%s" % pyqtcfg.pyqt_mod_dir)
-    print("pyqt_sip_dir:%s" % pyqtcfg.pyqt_sip_dir)
-    print("pyqt_sip_flags:%s" % pyqtcfg.pyqt_sip_flags)
-    print("pyqt_bin_dir:%s" % pyqtcfg.pyqt_bin_dir)
-else:
-    import sipconfig
-    sipcfg = sipconfig.Configuration()
-    print("pyqt_mod_dir:%s" % sipcfg.default_mod_dir)
-    print("pyqt_sip_dir:%s" % sipcfg.default_sip_dir)
-    print("pyqt_sip_flags:%s" % pyqt_sip_flags)
-    print("pyqt_bin_dir:%s" % sipcfg.default_bin_dir)
+        default_sip_dirs = [
+            '/usr/share/sip/Py2-PyQt4',
+            '/usr/share/sip/PyQt4',
+            '/usr/local/share/sip/PyQt4',
+            ]
+    for sip_dir in default_sip_dirs:
+        if os.path.exists(sip_dir) and os.path.exists(
+                os.path.join(sip_dir, 'QtCore')):
+            return sip_dir
+    raise ValueError('Cannot find SIP directory in default locations')
 
 
+def get_qt4_tag(sip_flags):
+    in_t = False
+    for item in sip_flags.split(' '):
+        if item == '-t':
+            in_t = True
+        elif in_t:
+            if item.startswith('Qt_4'):
+                return item
+        else:
+            in_t = False
+    raise ValueError('Cannot find Qt\'s tag in PyQt4\'s SIP flags.')
 
+
+if __name__ == '__main__':
+    
+    try:
+        import PyQt4.pyqtconfig
+        pyqtcfg = PyQt4.pyqtconfig.Configuration()
+        sip_dir = pyqtcfg.pyqt_sip_dir
+        sip_flags = pyqtcfg.pyqt_sip_flags
+    except ImportError:
+        # PyQt4 >= 4.10.0 was built with configure-ng.py instead of
+        # configure.py, so pyqtconfig.py is not installed.
+        sip_dir = get_default_sip_dir()
+        sip_flags = PyQt4.QtCore.PYQT_CONFIGURATION['sip_flags']
+
+    print('pyqt_version:%06.x' % PyQt4.QtCore.PYQT_VERSION)
+    print('pyqt_version_str:%s' % PyQt4.QtCore.PYQT_VERSION_STR)
+    print('pyqt_version_tag:%s' % get_qt4_tag(sip_flags))
+    print('pyqt_sip_dir:%s' % sip_dir)
+    print('pyqt_sip_flags:%s' % sip_flags)
